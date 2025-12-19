66ccccb38567ca6028c1de41df63a67dd2062d82
HaiderShaikhahmed <basicartgame@gmail.com>
2025-12-19 08:06:58 +0500

    Your descriptive commit message

--- loop.py
+++ loop.py
@@ -2,3 +2,11 @@
 no = int(input("Enter the no: "))
 for i in range (1,11): 
     print(no,"x",i,"=",no*i) 
+
+
+# while loop to print table
+no1= int(input("Enter the no : "))
+i=1
+while i<=10:
+    print(no1,"x",i,"=",no1*i)
+    i+=1
